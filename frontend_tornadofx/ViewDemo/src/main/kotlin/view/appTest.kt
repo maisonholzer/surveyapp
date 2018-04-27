a169336f06b0471d3918f5c244965799ad513b3e
Irons, Blake <blake-irons@uiowa.edu>
2018-04-27 00:22:41 -0500

    create new user/server communication

--- frontend_tornadofx/ViewDemo/src/main/kotlin/view/appTest.kt
+++ frontend_tornadofx/ViewDemo/src/main/kotlin/view/appTest.kt
@@ -4,10 +4,21 @@ import tornadofx.*
 import app.Styles.Companion.mainStyle
 import javafx.geometry.Orientation
 import javafx.geometry.Pos
+import javax.ws.rs.client.ClientBuilder
+import javax.ws.rs.client.Entity
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+import javax.ws.rs.client.ClientRequestContext
+import javax.ws.rs.client.ClientRequestFilter
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.glassfish.jersey.filter.LoggingFilter
+import java.net.PasswordAuthentication
+import javax.ws.rs.ext.ContextResolver
+import models.User
 
 class appTest : View() {
     override val root = Form().addClass(mainStyle)
-
 
     init {
         //title = "Survey Login"
@@ -25,21 +36,18 @@ class appTest : View() {
                 center = vbox {
                     alignment = Pos.CENTER
 
-                    fieldset {
-                        labelPosition = Orientation.VERTICAL
 
-                        field("Username") {
-                            textfield()
+                    val name = textfield()
+                    val pass = passwordfield {
 
-                        }
-
-                        field("Password") {
-                            passwordfield {  }
-
-                        }
                     }
 
                     //buttons for login screen
+                    button("New User") {
+                        action {
+                            createUser(name.getText(),pass.getText())
+                        }
+                    }
                     button("Log in") {
                         setOnAction {
                             println("does not work in test view")
@@ -61,4 +69,17 @@ class appTest : View() {
             }
         }
     }
+}
+
+fun createUser(n:String,p: String): Response {
+    val client = ClientBuilder.newClient()
+    val newUser = User(n,p)
+    val mapper = ObjectMapper()
+    val jsonString = mapper.writeValueAsString(newUser)
+    val resp = client
+            .target("http://localhost:8080/users")
+            .path("create")
+            .request(MediaType.APPLICATION_JSON)
+            .post(Entity.entity(jsonString, MediaType.APPLICATION_JSON))
+    return resp
 }
